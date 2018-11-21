bef992f7206c6a4135ba37bde0fa2de7b9f81c70
i.baydakov <devnull@localhost>
2018-11-21 21:24:53 +0300

    Fix assert condition

--- plus/Protocol-WebSocket-XS/src/panda/protocol/websocket/DeflateExt.h
+++ plus/Protocol-WebSocket-XS/src/panda/protocol/websocket/DeflateExt.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <cassert>
 #include <panda/refcnt.h>
 #include <panda/optional.h>
 #include <panda/protocol/websocket/HTTPRequest.h>
@@ -44,7 +44,7 @@ public:
     template<typename It>
     It compress(It payload_begin, It payload_end, bool final) {
         // we should not try to compress empty last piece
-        assert(!final && (payload_begin != payload_end));
+        assert(!(final && (payload_begin == payload_end)));
 
         It it_in = payload_begin;
         It it_out = payload_begin;
