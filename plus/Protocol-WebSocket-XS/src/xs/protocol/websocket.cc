1dd892ff4331df8c8ab7bfbbca78903f6ef881b3
syber <devnull@localhost>
2019-08-20 18:33:41 +0300

    remove aTHX pTHX from everywhere

--- plus/Protocol-WebSocket-XS/src/xs/protocol/websocket.cc
+++ plus/Protocol-WebSocket-XS/src/xs/protocol/websocket.cc
@@ -2,7 +2,7 @@
 
 namespace xs { namespace protocol { namespace websocket {
 
-void av_to_header_values (pTHX_ const Array& av, HTTPPacket::HeaderValues* vals) {
+void av_to_header_values (const Array& av, HTTPPacket::HeaderValues* vals) {
     if (!av.size()) return;
     vals->reserve(av.size());
     for (const auto& sv : av) {
@@ -11,14 +11,14 @@ void av_to_header_values (pTHX_ const Array& av, HTTPPacket::HeaderValues* vals)
         auto namesv = subav.fetch(0);
         if (!namesv) continue;
         HTTPPacket::HeaderValue elem;
-        elem.name = xs::in<string>(aTHX_ namesv);
+        elem.name = xs::in<string>(namesv);
         Hash args = subav.fetch(1);
-        if (args) for (const auto& row : args) elem.params.emplace(string(row.key()), xs::in<string>(aTHX_ row.value()));
+        if (args) for (const auto& row : args) elem.params.emplace(string(row.key()), xs::in<string>(row.value()));
         vals->push_back(std::move(elem));
     }
 }
 
-Array header_values_to_av (pTHX_ const HTTPPacket::HeaderValues& vals) {
+Array header_values_to_av (const HTTPPacket::HeaderValues& vals) {
     if (!vals.size()) return Array();
     auto ret = Array::create(vals.size());
     for (const auto& elem : vals) {
@@ -36,18 +36,18 @@ Array header_values_to_av (pTHX_ const HTTPPacket::HeaderValues& vals) {
     return ret;
 }
 
-void http_packet_set_headers (pTHX_ HTTPPacket* p, const Hash& hv) {
+void http_packet_set_headers (HTTPPacket* p, const Hash& hv) {
     p->headers.clear();
-    for (const auto& row : hv) p->headers.emplace(string(row.key()), xs::in<string>(aTHX_ row.value()));
+    for (const auto& row : hv) p->headers.emplace(string(row.key()), xs::in<string>(row.value()));
 }
 
-void http_packet_set_body (pTHX_ HTTPPacket* p, const Simple& sv) {
+void http_packet_set_body (HTTPPacket* p, const Simple& sv) {
     p->body.clear();
-    auto newbody = xs::in<string>(aTHX_ sv);
+    auto newbody = xs::in<string>(sv);
     if (newbody.length()) p->body.push_back(newbody);
 }
 
-Simple strings_to_sv (pTHX_ const string& s1, const string& s2) {
+Simple strings_to_sv (const string& s1, const string& s2) {
     auto len = s1.length() + s2.length();
     if (!len) return Simple::undef;
 
@@ -60,10 +60,10 @@ Simple strings_to_sv (pTHX_ const string& s1, const string& s2) {
     return ret;
 }
 
-void av_to_vstring (pTHX_ const Array& av, std::vector<string>& v) {
+void av_to_vstring (const Array& av, std::vector<string>& v) {
     for (const auto& elem : av) {
         if (!elem.defined()) continue;
-        v.push_back(xs::in<string>(aTHX_ elem));
+        v.push_back(xs::in<string>(elem));
     }
 }
 
